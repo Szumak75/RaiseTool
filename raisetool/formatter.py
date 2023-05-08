858c8f808164c0e7e5a5b8a58b29ffddc593afd1
Jacek Kotlarski <szumak@virthost.pl>
2023-05-08 22:13:32 +0200

    Add Test class and Raise class.

---
+++ raisetool/formatter.py
@@ -0,0 +1,15 @@
+# -*- coding: UTF-8 -*-
+"""
+  Author:  Jacek Kotlarski --<szumak@virthost.pl>
+  Created: 08.05.2023
+
+  Purpose:
+"""
+from attribtool.ndattrib import NoDynamicAttributes
+
+
+class Raise(NoDynamicAttributes):
+    """Raise class for formatting thrown exception messages."""
+
+
+# #[EOF]#######################################################################
---
+++ tests/test_class.py
@@ -0,0 +1,21 @@
+# -*- coding: UTF-8 -*-
+"""
+  Author:  Jacek Kotlarski --<szumak@virthost.pl>
+  Created: 08.05.2023
+
+  Purpose:
+"""
+
+import unittest
+
+from raisetool.formatter import Raise
+
+
+class TestRaise(unittest.TestCase):
+    """Tests for Raise class."""
+
+    def test_create_class_instance(self):
+        """Test nr 1."""
+
+
+# #[EOF]#######################################################################
