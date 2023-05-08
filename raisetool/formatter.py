cfcbf3821b513854e81d8185120d7926ccf32149
Jacek Kotlarski <szumak@virthost.pl>
2023-05-08 23:47:14 +0200

    message method & tests

--- raisetool/formatter.py
+++ raisetool/formatter.py
@@ -5,11 +5,40 @@
 
   Purpose:
 """
+
+from types import FrameType
+from typing import Optional
 from attribtool.ndattrib import NoDynamicAttributes
 
 
 class Raise(NoDynamicAttributes):
     """Raise class for formatting thrown exception messages."""
+
+    @classmethod
+    def message(
+        cls,
+        message: str,
+        class_name: str = "",
+        currentframe: Optional[FrameType] = None,
+    ) -> str:
+        """Message formatter method.
+
+        message: str    - message to format
+        class_name: str - caller class name (self.__class__.__name__)
+        currentframe: FrameType - object from inspect.currentframe()
+
+        Return: formatted message string
+        """
+        template = f"{message}"
+        if currentframe and isinstance(currentframe, FrameType):
+            template = f"{currentframe.f_code.co_name} [line:{currentframe.f_lineno}]: {template}"
+        elif isinstance(class_name, str) and class_name != "":
+            template = f"{class_name}: {template}"
+            return template
+        else:
+            return template
+        template = f"{class_name}.{template}"
+        return template
 
 
 # #[EOF]#######################################################################
--- tests/test_class.py
+++ tests/test_class.py
@@ -7,6 +7,7 @@
 """
 
 import unittest
+import inspect
 
 from raisetool.formatter import Raise
 
@@ -16,6 +17,39 @@ class TestRaise(unittest.TestCase):
 
     def test_create_class_instance(self):
         """Test nr 1."""
+        obj = Raise()
+        self.assertTrue(obj, Raise)
+
+    def test_returning_message_from_instance(self):
+        """Test nr 2."""
+        obj = Raise()
+        self.assertEqual(obj.message("test"), "test")
+        self.assertEqual(
+            obj.message("test", self.__class__.__name__), "TestRaise: test"
+        )
+        self.assertEqual(
+            obj.message(
+                "test",
+                class_name=self.__class__.__name__,
+                currentframe=inspect.currentframe(),
+            ),
+            "TestRaise.test_returning_message_from_instance [line:31]: test",
+        )
+
+    def test_returning_message_from_class(self):
+        """Test nr 3."""
+        self.assertEqual(Raise.message("test"), "test")
+        self.assertEqual(
+            Raise.message("test", self.__class__.__name__), "TestRaise: test"
+        )
+        self.assertEqual(
+            Raise.message(
+                "test",
+                class_name=self.__class__.__name__,
+                currentframe=inspect.currentframe(),
+            ),
+            "TestRaise.test_returning_message_from_class [line:46]: test",
+        )
 
 
 # #[EOF]#######################################################################
